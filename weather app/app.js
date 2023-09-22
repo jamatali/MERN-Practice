9c88d670d70f4a4306198bef62f240d1fe7eb0bb
Jamat Ali <engr.jamatali@gmail.com>
2023-09-22 16:06:29 +0500

    Weather App completed

--- weather app/app.js
+++ weather app/app.js
@@ -1,38 +1,40 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
 const port = 3000;
 
-app.get("/", (req,res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-res.sendFile(__dirname + "/index.html");
-})
-
-// const query = "London";
-// const apikey = "cb2baa288e1527c53508f3cb2a8aca67";
-// const unit = "metric";
-// const iurl = "https://api.openweathermap.org/data/2.5/weather"
-// const url = iurl + "?q=" + query + "&appid=" + apikey + "&units=" + unit;
-// https.get(url, (response) =>{
-//     console.log(response.statusCode);
-//     response.on("data", (data)=>{
-//         const weatherData = JSON.parse(data);
-//         const temp =weatherData.main.temp;
-//         const weatherDiscription = weatherData.weather[0].description;
-//         const icon = weatherData.weather[0].icon;
-//         const url_icon = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-//         console.log(weatherData);
-//         console.log(weatherDiscription);
-//         console.log("Londaon Temp: " + temp);
-//         // res.send(weatherData);
-//         res.write(`<p>Details are  ${weatherDiscription}<p>`);
-//         res.write(`<h1>The London Temperature right now is: ${temp} degrees<h1>`)
-//         res.write(`<img src=${url_icon} />`)
-//         res.send();
-//     })
-// })
-// // res.send("hello")
+app.post("/", (req, res) => {
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const apikey = "cb2baa288e1527c53508f3cb2a8aca67";
+  const unit = "metric";
+  const url ="https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
+  https.get(url, (response) => {
+    console.log(response.statusCode);
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDiscription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const url_icon = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      console.log(weatherData);
+      console.log(weatherDiscription);
+      console.log("Londaon Temp: " + temp);
+      // res.send(weatherData);
+      res.write(`<p>Details are  ${weatherDiscription}<p>`);
+      res.write(`<h1>The ${query} Temperature right now is: ${temp} degrees<h1>`);
+      res.write(`<img src=${url_icon} />`);
+      res.send();
+    });
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Weather App Server has started on port: ${port}`)
-})
+  console.log(`Weather App Server has started on port: ${port}`);
+});
