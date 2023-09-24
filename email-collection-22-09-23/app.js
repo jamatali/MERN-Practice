7b8f83aa6c1c0b2c80eb9b6f5d8774234dffc1c4
Jamat Ali <engr.jamatali@gmail.com>
2023-09-24 23:44:51 +0500

    Post request successful

--- email-collection-22-09-23/app.js
+++ email-collection-22-09-23/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", (req,res)=>{
-    console.log("post request Respons: " + req)
-    // const fname = req.body.fname;
-    // const lname = req.body.lname;
-    // const email = req.body.email;
-    // console.log("Your First Name: " + fname);
-    // console.log("Your Last Name: " + lname);
-    // console.log("Your Email Id: " + email);
+    console.log("post request Respons1: " + req.body.fname);
+    console.log("post request Respons2: " + req.body.lname);
+    console.log("post request Respons3: " + req.body.email);
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
+    console.log("Your First Name: " + fname);
+    console.log("Your Last Name: " + lname);
+    console.log("Your Email Id: " + email);
+    res.send("ok");
     
 })
 
