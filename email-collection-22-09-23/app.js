3311ba254508e1a6b8f838ae06f6b42741de5b74
Jamat Ali <engr.jamatali@gmail.com>
2023-09-26 12:20:23 +0500

    updates

--- email-collection-22-09-23/app.js
+++ email-collection-22-09-23/app.js
@@ -2,27 +2,28 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/index.html");
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post("/", (req,res)=>{
-    console.log("post request Respons1: " + req.body.fname);
-    console.log("post request Respons2: " + req.body.lname);
-    console.log("post request Respons3: " + req.body.email);
-    const fname = req.body.fname;
-    const lname = req.body.lname;
-    const email = req.body.email;
-    console.log("Your First Name: " + fname);
-    console.log("Your Last Name: " + lname);
-    console.log("Your Email Id: " + email);
-    res.send(`You Have Entered First Name: ${fname}, Last Name: ${lname} and Email: ${email}`);
-})
-
+app.post("/", (req, res) => {
+  // console.log("post request Respons1: " + req.body.fname);
+  // console.log("post request Respons2: " + req.body.lname);
+  // console.log("post request Respons3: " + req.body.email);
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  // console.log("Your First Name: " + fname);
+  // console.log("Your Last Name: " + lname);
+  // console.log("Your Email Id: " + email);
+  res.send(
+    `You Have Entered First Name: ${fname}, Last Name: ${lname} and Email: ${email}`
+  );
+});
 
 app.listen(port, () => {
-    console.log(`Email Collection App is running on Port: ${port}`)
-})
+  console.log(`Email Collection App is running on Port: ${port}`);
+});
